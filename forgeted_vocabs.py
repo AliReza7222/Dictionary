a95e671d4cb84d5510adc3dfa1773734784ce31f
seyed alireza <ali.r.salehi99@gmail.com>
2022-01-05 13:54:35 +0330

    commit for rm

---
+++ forgeted_vocabs.py
@@ -0,0 +1,60 @@
+###b_kh
+###that vocabes  i forgeted
+#import pdb
+
+from my_vocab import vocab
+
+class vocab_forget(vocab):
+  def __init__(self):
+
+    super().__init__()
+
+    self.list_vocabes=['strength','weaknees','decline','invitation','busy','quite','alternative','pair','situation',
+    'unanimous','achieve','benefit','nickname','discuss','Decision','divide','describe','destination','each',
+    'reach']
+
+    self.dic_forget={'famine':['is noun' ,'drought','scarcity'],'surpass':['is verb','top','better','exceed'],
+    'accused':['is adjective '],'deluded':['is verb','duped'],'proposal':['is noun','bid','offer'],
+    'odds':['is noun','probability'],'loot':['is noun and verb used without object ','robbery','plunder'],
+    'refuses':['is verb', 'deny','ignore'],'stupid':['is adjective','dumb','foolish'],
+    'sensational':['is adjective','exciting','thriling'],'urgent':["is adjective",'important','essential'],
+    'hazy':['is adjective','confused','vague'],'**foresee':[' is verb','predict','anticipate','discern'],
+    'inspect':['is verb','verify','check'],'late':['is adjective',"late != early"],'gossip':['is noun and verb','talk about others','rumor'],
+    'neighbor':['is noun','person who lives close by','acquaintance'],'steer':['is verb','guide','drive'],
+    'exsampl':['is noun','like'],'*prisoner':['is noun','captive','convict'],'defiant':['is adjective'],
+    'deceive':['is verb','trick','dupe'],'daring':['is adjective','boldness','adventurous','reckless'],
+    'corpse':['is noun','cadaver'],'panic':['is noun','fear','dread'],'swindler':['is noun','bandit','thief'],
+    'impressed':['is verb','thrill','influence'],'dizzy':['is adjective','confused','crazy'],
+    'unaccustomed':['is adjective','unfamiliar'],'distracted':['is adjective','troubled'],
+    'minority':['is noun','few something','the outnumbered'],'*brawl':['is noun','nasty fight','fight','altercation'],
+    'brutal':['is adjective','cruel','remorseless'],'innovative':['is adjective','creative','ingenious'],
+    'dilemma':['is noun','difficulty','problem'],'childhood':['is noun','period of being young'],
+    'issue':['is noun','question'],'got':['is verb','receive','take'],'result':['is noun'],
+    'politely':['is adverb','person courteous'],'Introducing':['is verb','make know,present'],
+    '*Intraction':['is noun','communication'],'equal':['is noun','peer','match'],
+    'opportunity':['is noun','chance','lucky'],'beneficiary':['is noun','person who recives benefit'],
+    'delinquent':['is noun', 'criminal','offendr','felon'],'deprive':['is verb','take away from by force'],
+    'homicide':['is noun','murder','killing'],'justice':['is noun','fairness','fear dealing'],
+    'penalize':['is verb','punish','atoned'],'reject':['is verb','refuse','deny'],'spouse':['is noun','husband','wife'],
+    'unstable':['is adjective','fickle'],'vocation':['is noun','occupatin','job'],'warden':['is noun','boss','administrator','guard'],
+    'embrace':['is verb','hug one another'],'exhibit':['is verb','show','display'],'extract':['is verb'],
+    'forbid':['is verb','prohibit','ban'],'logical':['is adj','reasonable'],'partial':['is adj','incomplete','not total'],
+    '*precaution':['is noun','foresight','carefulness'],'prior':['is adj','previous','earlier'],
+    '*proceed':['is verb','pass','go on','continue','move forward'],'Rarely':['is adj','not often','seldom'],
+    'Reptile':['is as'], 'give':['is verb'],'valiant':['is adj','brave','courageous'],'Authority':['is noun','power'],
+    'consider':['is verb','think','verify'],'Detest':['is verb','hate'],'Encourage':['is verb','increase the confidence'],
+    '*Fierce':['is adj','violent','savage','wild'],'neutral':['is adj','impartial'],'scowl':['is noun','frown'],
+    'sneer':['is verb','jeer','mock'],'trifle':['is verb'],'vermin':['is noun','dishonoraible animal'],
+    'wail':['is verb','cry loud'],'Abroad':['is adj'],'architect':['is non'],'baggage':['is noun'],
+    'calamity':['is noun','disaster','tragedy'],'collapse':['is noun','breakdown','downfall'],
+    'Envy':['is noun','jealousy'],'fugitive':['is noun','escapee','away'],'matrimony':['is noun'],
+    'pauper':['is noun','person who is poor'],'*prosecute':['is verb'],'squander':['is verb','use up','waste'],
+    'awesome':['is adj','amazing','wonder'],'awkward':['is adj','clumsy','embarrassing'],
+    'beckon':['is verb','call\signal with leg or hand or...'],'betray':['is verb','disloyal'],
+    'compel':['is verb','force to act'],'guide':['is verb','lead'],'pacify':['is verb','make peaceful','appease'],
+    'possible':['is adj'],'*Quench':['is verb'],'Respond':['is verb','answer','react'],'utter':['is adj'],
+    'venture':['is noun','gamble','business risk'],'cliff':['is noun'],'scene':['is noun','display'],
+    'fate':['is noun'],'*fur':['is nonu','skin animals','coat'],'realm':['is noun','area','kingdom'],
+    'reach':['is verb'],'cease':['is verb','stop'],'despite':['is prep'],'disrupt':['is verb','disorganize','upset'],
+    'Exhaust':['is verb','exit','exhaust'],'later':['is adj','coming after']}
+
