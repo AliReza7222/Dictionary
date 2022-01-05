351287d468640273ca1a4c37d77b668629134eb7
seyed alireza <ali.r.salehi99@gmail.com>
2022-01-05 13:53:33 +0330

    complate first change project

--- run_vocabs.py
+++ run_vocabs.py
@@ -3,34 +3,47 @@
 from datetime import datetime
 from main import Config
 
-# from forgeted_vocabs import vocab_forget
-
 vocabs = Config()
 
 
-print('#--------------- start first vocabs ------------------#')
-# number_vocab = 0
-# with open("my_vocab.txt", "w") as file:
-#     for vocab in vocabs.my_list_vocab:
-#
-#         number_vocab += 1
-#         print('('+str(number_vocab)+')' + '---->' + vocab+'\n')
-#         file.write('('+str(number_vocab)+')' + '---->' + vocab+'\n')
+def time_enter_exit(key):
+    with open("days that i work enghlish vocabs.txt", "a") as file:
+        time = datetime.now()
+        if key == "enter":
+            line = f"preson enter at time : {time.strftime('%A')} {time} \n"
+        elif key == 'exit':
+            line = f"preson exit at time : {time.strftime('%A')} {time} \n\n"
+        file.write(line)
 
 
-print('********** let\'s go for learn vocabs english *************')
-# number_vocab = 0
-# with open("forget_vocab.txt", "w") as file:
-#     for vocab in vocabs.dic_forget:
-#         number_vocab += 1
-#         # file.write(str(number_vocab) + '_' + vocab + ': ' + str(vocabs.dic_forget[vocab]) + "\n")
-#         try:
-#             if int(input("Press key enter. for out of program enter 0 : ")) == 0:
-#                 print("exit of program...\ngood lock")
-#                 break
-#         except:
-#             print("\n"+str(number_vocab) + '_' + vocab + ': ' + str(vocabs.dic_forget[vocab])+"\n")
+enter = input("vocab's old or forget: (forget/old) ? ")
+if enter == "old":
+    time_enter_exit("enter")
+    print('#--------------- start first vocabs ------------------#')
+    list_vocab = vocabs.old_vocabs()
+    num = 0
+    for v in list_vocab:
+        num += 1
+        try:
+            if int(input("show vocab with key Enter and exit of program with key 0 : ")) == 0:
+                print("exit of program\ngood luck.")
+                break
+        except:
+            print(f"\n\t{num}_{v}\n")
+elif enter == "forget":
+    time_enter_exit("enter")
+    print('#--------------- start first vocabs ------------------#')
+    list_vocab = vocabs.forget_vocab()
+    num = 0
+    for k, v in zip(list_vocab.keys(), list_vocab.values()):
+        num += 1
+        try:
+            if int(input("show vocab with key Enter and exit of program with key 0 : ")) == 0:
+                print("exit of program\ngood luck.")
+                time_enter_exit("exit")
+                break
+        except:
+            print(f"\n\t{num}_{k} =\t{v}\n")
 
-# with open("days that i work enghlish vocabs.txt", "a") as file:
-#     text = f"date is ---->{str(datetime.now())}\t vocab: {number_vocab}.\n"
-#     file.write(text)
+else:
+    print("Error enter....")
